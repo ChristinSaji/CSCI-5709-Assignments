b6bfe3a1170ea85b6d94c40da3812f52fc706934
Christin Saji <ch580848@dal.ca>
2024-06-05 09:55:42 -0300

    add custom colors

--- Assignment1/user-profile-management/tailwind.config.js
+++ Assignment1/user-profile-management/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: "#03045e",
+        blue: "#0077b6",
+        skyBlue: "#00b4d8",
+        lightBlue: "#90e0ef",
+        veryLightBlue: "#caf0f8",
+      },
+    },
   },
   plugins: [],
 };
